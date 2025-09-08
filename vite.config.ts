55d15f10e92d8eee45e62f08fd97bdf8857b435d
vamshipmscrt-lang <vamshipmscrt@gmail.com>
2025-09-08 16:45:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+     base: '/pm-project/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
